940f48398744f24d23a7998eb7a8501009b34c88
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2021-09-14 01:26:55 +0600

    laravel custom errors pages create

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos = Todo::all();
+        return view('modules.todo.if_else', compact('todos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Todo::create([
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
+        return redirect('todo');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        //
+    }
+}
---
+++ resources/views/modules/todo/if_else.blade.php
@@ -0,0 +1,46 @@
+@extends('welcome')
+@section('cotnent')
+
+<section class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <h4>Create Form</h4>
+            <form action="{{ url('todo') }}" method="POST" class="form-group">
+           
+                @method('POST')
+                <input type="text" name="title" placeholder="title" class="form-control">
+                <textarea name="description" id="" cols="10" placeholder="description" rows="4" class="form-control"></textarea>
+                <input type="submit" class="btn btn-sm bg-primary" value="Submit">
+            </form>
+        </div>
+        <div class="col-md-8">
+            <h4>Todo Table</h4>
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Index</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @forelse ($todos as $item)
+                    <tr>
+                        <th scope="row">{{ $item->id }}</th>
+                        <td>{{ $item->title }}</td>
+                        <td>{{ $item->description }}</td>
+                        <td>
+                            <span class="">active</span>
+                        </td>
+                      </tr>
+                    @empty
+
+                    @endforelse
+                </tbody>
+              </table>
+        </div>
+    </div>
+</section>
+
+@endsection
